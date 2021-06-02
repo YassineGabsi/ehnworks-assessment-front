2e32b2ba9dde5cbb1dd5158e6f5382b1900e8b84
PsycoCast <gabsiyassine@gmail.com>
2021-06-02 09:03:09 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import React from "react";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/' component={Login}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
