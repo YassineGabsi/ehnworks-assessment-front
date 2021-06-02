977aeaef7cbbe2870e5b120919f66e4e633fb395
PsycoCast <gabsiyassine@gmail.com>
2021-06-02 09:22:59 +0100

    form styling

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,8 +3,11 @@ import "./Login.css";
 
 function Login() {
     return (
-        <div>
-            Login
+        <div className="form-wrapper">
+            <p>Create your Account</p>
+            <input className="form-input mt-4" placeholder="Email Address" type="text"/>
+            <input className="form-input mt-4" placeholder="Password" type="text"/>
+            <button className="form-button mt-4">LOGIN</button>
         </div>
     );
 }
