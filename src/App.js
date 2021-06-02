be883916a8cd4a9db05e3ac75e759db2c20a7d13
PsycoCast <gabsiyassine@gmail.com>
2021-06-02 09:52:23 +0100

    call to login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import axios from 'axios';
 
-import React from "react";
+import React, {useState} from "react";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+
 function App() {
-  return (
-    <div className="app-wrapper align-items-center justify-content-center text-center d-flex">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='/register' component={Register}/>
-          <Route exact path='/' component={Login}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    const [name, setName] = useState("");
+
+    const postLogin = (email, password) => {
+        const login = new FormData();
+        login.append('email', email);
+        login.append('password', password);
+        axios.post(`http://localhost:8000/api/login`, login)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    };
+
+    const goToLogin = () => {
+        return (
+            <Login postLogin={postLogin}/>
+        );
+    };
+    return (
+        <div className="app-wrapper align-items-center justify-content-center text-center d-flex">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/login' component={goToLogin}/>
+                    <Route exact path='/register' component={Register}/>
+                    <Route exact path='/' component={Login}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
-function Login() {
+function Login({ postLogin }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        postLogin(email, password);
+
+    };
+
     return (
         <div className="form-wrapper">
             <p>Create your Account</p>
-            <input className="form-input mt-4" placeholder="Email Address" type="text"/>
-            <input className="form-input mt-4" placeholder="Password" type="text"/>
-            <button className="form-button mt-4">LOGIN</button>
+            <form onSubmit={handleSubmit} style={{display: "grid"}}>
+                <input value={email}
+                       onChange={e => setEmail(e.target.value)}
+                       className="form-input mt-4"
+                       name="email"
+                       required
+                       placeholder="Email Address"
+                       type="text"/>
+                <input value={password}
+                       onChange={e => setPassword(e.target.value)}
+                       className="form-input mt-4"
+                       name="password"
+                       required
+                       placeholder="Password" type="password"/>
+                <button type="submit" className="form-button mt-4">LOGIN</button>
+            </form>
+
         </div>
     );
 }
