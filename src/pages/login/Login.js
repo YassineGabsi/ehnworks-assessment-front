63d5b69a32e0745a6f9a5e992d2ff1640dcf2f39
PsycoCast <gabsiyassine@gmail.com>
2021-06-02 10:00:10 +0100

    validate login formé

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -11,10 +11,17 @@ function Login({ postLogin }) {
 
     };
 
+    const validateEmail = (emailAdress) => {
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return !!emailAdress.match(regexEmail);
+    };
+
     return (
         <div className="form-wrapper">
             <p>Create your Account</p>
             <form onSubmit={handleSubmit} style={{display: "grid"}}>
+                { email.length !==0 && !validateEmail(email)?
+                    (<div className="error-input">Please provide a valid email</div>) : null}
                 <input value={email}
                        onChange={e => setEmail(e.target.value)}
                        className="form-input mt-4"
@@ -22,13 +29,15 @@ function Login({ postLogin }) {
                        required
                        placeholder="Email Address"
                        type="text"/>
+                { password.length !==0 && password.length <= 4 ?
+                    (<div className="error-input mt-2">Please provide a valid Password</div>) : null}
                 <input value={password}
                        onChange={e => setPassword(e.target.value)}
                        className="form-input mt-4"
                        name="password"
                        required
                        placeholder="Password" type="password"/>
-                <button type="submit" className="form-button mt-4">LOGIN</button>
+                <button type="submit" disabled={!validateEmail(email) || password.length <= 4} className="form-button mt-4">LOGIN</button>
             </form>
 
         </div>
